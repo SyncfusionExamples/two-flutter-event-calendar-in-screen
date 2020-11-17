6be9aa5ebd642ee03f74517a8a998438aa929811
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-17 16:45:56 +0530

    FLUT-2658 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/flutter_twocalendar_vertical/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_twocalendar_vertical
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(TwoCalendars());
+
+class TwoCalendars extends StatefulWidget {
+  @override
+  TwoCalendarsState createState() => TwoCalendarsState();
+}
+
+class TwoCalendarsState extends State<TwoCalendars> {
+  CalendarController _calendarController1, _calendarController2;
+
+  @override
+  void initState() {
+    _calendarController1 = CalendarController();
+    _calendarController2 = CalendarController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SafeArea(
+          child: Column(
+            children: [
+              SfCalendar(
+                view: CalendarView.month,
+                controller: _calendarController1,
+                dataSource: _getDataSource(),
+                monthViewSettings: MonthViewSettings(
+                  navigationDirection: MonthNavigationDirection.vertical,
+                ),
+                onViewChanged: calendar1ViewChanged,
+              ),
+              SfCalendar(
+                view: CalendarView.month,
+                controller: _calendarController2,
+                viewHeaderHeight: 0,
+                monthViewSettings: MonthViewSettings(
+                    navigationDirection: MonthNavigationDirection.vertical),
+                dataSource: _getDataSource(),
+                onViewChanged: calendar2ViewChanged,
+              ),
+            ],
+          ),
+        )));
+  }
+
+  void calendar1ViewChanged(ViewChangedDetails viewChangedDetails) {
+    SchedulerBinding.instance.addPostFrameCallback((Duration duration) {
+      _calendarController2.displayDate = DateTime(
+          _calendarController1.displayDate.year,
+          _calendarController1.displayDate.month + 1,
+          _calendarController1.displayDate.day);
+    });
+  }
+
+  void calendar2ViewChanged(ViewChangedDetails viewChangedDetails) {
+    SchedulerBinding.instance.addPostFrameCallback((Duration duration) {
+      _calendarController1.displayDate = DateTime(
+          _calendarController2.displayDate.year,
+          _calendarController2.displayDate.month - 1,
+          _calendarController2.displayDate.day);
+    });
+  }
+
+  _DataSource _getDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 4)),
+      endTime: DateTime.now().add(Duration(hours: 5)),
+      subject: 'Meeting',
+      color: Colors.red,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 5)),
+      subject: 'Development Meeting   New York, U.S.A',
+      color: Color(0xFFf527318),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 4)),
+      subject: 'Project Plan Meeting   Kuala Lumpur, Malaysia',
+      color: Color(0xFFfb21f66),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 3)),
+      subject: 'Support - Web Meeting   Dubai, UAE',
+      color: Color(0xFFf3282b8),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(days: -2, hours: 1)),
+      endTime: DateTime.now().add(Duration(days: -2, hours: 2)),
+      subject: 'Project Release Meeting   Istanbul, Turkey',
+      color: Color(0xFFf2a7886),
+    ));
+    appointments.add(Appointment(
+        startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+        endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+        subject: 'Release Meeting',
+        color: Colors.lightBlueAccent,
+        isAllDay: true));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2, days: -4)),
+      endTime: DateTime.now().add(const Duration(hours: 4, days: -4)),
+      subject: 'Performance check',
+      color: Colors.amber,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 11, days: -2)),
+      endTime: DateTime.now().add(const Duration(hours: 12, days: -2)),
+      subject: 'Customer Meeting   Tokyo, Japan',
+      color: Color(0xFFffb8d62),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+      subject: 'Retrospective',
+      color: Colors.purple,
+    ));
+
+    return _DataSource(appointments);
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
